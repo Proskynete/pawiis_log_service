60712fd7696e9e5dd54bbaab61b83ec28841c6ec
Eduardo Alvarez <eduardo.a1993@gmail.com>
2020-07-23 15:07:53 -0400

    Create new log

--- src/controllers/log.controller.js
+++ src/controllers/log.controller.js
@@ -1,5 +1,34 @@
 const Log = require('../models/log.model');
 
+const create = async (req, res) => {
+	const { action_type, description, pet_id, owner_id, executor_id } = req.body;
+
+	try {
+		const newLog = new Log({
+			action_type,
+			description,
+			pet_id,
+			owner_id,
+			executor_id,
+			created_at: Date.now(),
+			updated_at: Date.now(),
+			last_login: Date.now(),
+		});
+
+		const savedLog = await newLog.save();
+
+		res.status(201).json({
+			log: savedLog,
+		});
+	} catch (error) {
+		return res.status(500).json({
+			status: 500,
+			error,
+			message: `Server Error`,
+		});
+	}
+};
+
 const get = async (req, res) => {
 	const { pet_id } = req.query;
 	try {
@@ -27,4 +56,5 @@ const get = async (req, res) => {
 
 module.exports = {
 	get,
+	create,
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,5 +4,6 @@ const LogController = require('../controllers/log.controller');
 const routes = express.Router();
 
 routes.get('/get', LogController.get);
+routes.post('/create', LogController.create);
 
 module.exports = routes;
