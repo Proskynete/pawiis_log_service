cd42e592dc4cb3650b0a602a236d9a3335b66e72
Eduardo Alvarez <eduardo.a1993@gmail.com>
2020-07-23 13:17:04 -0400

    Change port

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	port: process.env.PORT || 5003,
+	port: process.env.PORT || 5002,
 	db: process.env.MONGODB || 'mongodb://localhost/pawii_log_service',
 	SECRET_TOKEN: 'ULx{ecGK[[h`qK3x',
 	saltRounds: 10,
